31d82f9f70a96a4304243d0f52d3f65cd4147b02
RomashkaDeveloper <forzaerfahren@outlook.com>
2025-09-15 20:47:07 +0300

    home dir fixed

---
+++ README.md
@@ -0,0 +1,19 @@
+# Installation
+
+```bash
+uv sync
+uv run main.py
+```
+
+# Compiling
+
+```bash
+uv run pyinstaller main.py
+```
+
+# Add config
+```bash
+uv run main.py config --api-key YOUR_OPENROUTER_API_KEY
+uv run main.py config --user-name YOUR_NAME
+uv run main.py config --character-name CHARACTER_NAME
+```
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,14 +1,19 @@
 import os
 import sys
 
-__home_dir__ = os.environ.get('HOME', "")
+__version__: str = "0.1.0"
+__shell__: str = os.environ.get("SHELL", "unknown") if os.name == "posix" else os.environ.get("COMSPEC", "unknown")
+__platform__:str = sys.platform
+
+if __platform__ in ["linux", "macos", "darwin"]:
+    __home_dir__: str = os.environ.get('HOME', "")
+elif __platform__ == "win32":
+    __home_dir__: str = os.path.join(os.environ.get("HOMEDRIVE", ""), os.environ.get("HOMEPATH", ""))
 
 assert os.path.exists(__home_dir__), "Путь к домашней папке не найден"
-config_path = os.path.join(__home_dir__, ".agent")
+config_path: str = os.path.join(__home_dir__, ".agent")
 os.makedirs(config_path, exist_ok=True)
-__config_file__ = os.path.join(config_path, "config.json")
+__config_file__: str = os.path.join(config_path, "config.json")
 
-__platform__ = sys.platform
-__shell__ = os.environ.get("SHELL", "unknown") if os.name == "posix" else os.environ.get("COMSPEC", "unknown")
-__user__ = "Рома"
-__character__ = "Ева"
+__user__: str = "user"
+__character__:str = "Ева"
